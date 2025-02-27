9c11bedc637f71d43fa4a506b0781de8943ccc73
Yoni Mor <yoni304@gmail.com>
2025-02-27 15:54:12 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div>hello gavriel</div>;
+  return (
+    <div className="w-screen">
+      <div className="h-[57px] flex items-end bg-[#00AEEF]">
+        <img
+          src="crosswords-logo 1.svg"
+          alt="crosswords-logo"
+          className="h-[48px]"
+        />
+        <div className="flex ml-8">
+          <img src="join-header-btn.svg" alt="" className="mb-4 w-[60px]" />
+          <img src="sign-in-pic.svg" alt="" className="mb-4 ml-2 " />
+          <img src="Sign In-writing.svg" alt="" className="mb-4 ml-2" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
