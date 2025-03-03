15f638fcf08e0adb70102577e42f25b524d0aa38
Yoni Mor <yoni304@gmail.com>
2025-03-03 20:08:17 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import Home from "./Home";
 function App() {
   return (
     <>
-    <Home/>
+      <Home />
       <div className="flex justify-center items-center bg-[#00AEEF] px-4 h-[41px] md:h-[93px]">
         <ul className="flex justify-center gap-1 text-[9px] md:text-xl md:gap-6">
-          <li>Home</li>
+          {/* Replace # with the actual link to Home*/}
+          <a href="#">
+            <li>Home</li>
+          </a>
           <li>|</li>
-          <li>Members Area</li>
+          {/* Replace # with the actual link to Members Area*/}
+          <a href="#">
+            <li>Members Area</li>
+          </a>
           <li>|</li>
-          <li>Terms & Conditions</li>
+          {/* Replace # with the actual link to Terms & Conditions*/}
+          <a href="#">
+            <li>Terms & Conditions</li>
+          </a>
           <li>|</li>
-          <li>Privacy</li>
+          {/* Replace # with the actual link to Privacy*/}
+          <a href="#">
+            <li>Privacy</li>
+          </a>
           <li>|</li>
-          <li>Billing Support</li>
+          {/* Replace # with the actual link to Billing Support*/}
+          <a href="#">
+            <li>Billing Support</li>
+          </a>
         </ul>
       </div>
 
@@ -28,47 +43,6 @@ function App() {
         <span>Copyright © 2018 MentalGameTime.com</span>
       </div>
     </>
-
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/privacy" element={<Privacy />} />
-    //   </Routes>
-
-    //   <div className="flex justify-center items-center bg-[#00AEEF] px-4 h-[41px] md:h-[93px]">
-    //     <ul className="flex justify-center gap-1 text-[9px] md:text-xl md:gap-6">
-    //       <li>
-    //         <Link to="/">Home</Link>
-    //       </li>
-    //       <li>|</li>
-    //       <li>
-    //         <Link to="/gavriel/members">Members Area</Link>
-    //       </li>
-    //       <li>|</li>
-    //       <li>
-    //         <Link to="/gavriel/terms">Terms & Conditions</Link>
-    //       </li>
-    //       <li>|</li>
-    //       <li>
-    //         <Link to="/gavriel/privacy">Privacy</Link>
-    //       </li>
-    //       <li>|</li>
-    //       <li>
-    //         <Link to="/gavriel/billing-support">Billing Support</Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-
-    //   <div className="mx-6 leading-6 text-[10px] text-center mt-4 mb-10 md:mb-14 md:text-2xl md:leading-10">
-    //     <span className="">
-    //       © Copyright 2025 Wheeler Solutions, LLC All Rights Reserved
-    //     </span>
-    //     <br />
-    //     <span>Wheeler Solutions, LLC 1262 Route 206 Shamong, NJ 08088</span>
-    //     <br />
-    //     <span>Copyright © 2018 MentalGameTime.com</span>
-    //   </div>
-    // </Router>
   );
 }
 
--- src/Privacy.tsx
+++
@@ -1,9 +0,0 @@
-function Privacy() {
-  return (
-    <div className="bg-amber-400 w-screen h-screen p-4">
-      This is the Privacy Policy.
-    </div>
-  );
-}
-
-export default Privacy;
