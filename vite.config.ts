b88113d6f64692d4fbb047508a9e9b84595315d0
Yoni Mor <yoni304@gmail.com>
2025-03-03 19:08:29 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // base: "/gavriel",// this needs to be turned on for production
   plugins: [react()],
   server: {
   },
